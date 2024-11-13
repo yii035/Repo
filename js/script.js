0e625356d2d354fa697e749ec52cd8fbc6891124
fclieu1234@gmail.com <fclieu1234@gmail.com>
2024-11-13 22:19:31 +0700

    loading source code

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".index-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear',
+        
+        
+    });
+  });
+
+
+  
